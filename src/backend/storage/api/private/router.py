acf2d71af209142ef2e27f034d53ad280fba2e53
Dror Kabely <sdrork12@campus.technion.ac.il>
2025-09-22 01:42:26 +0300

    [src/backend/storage] added delete endpoint for private files
    Signed-off-by: Dror Kabely <sdrork12@campus.technion.ac.il>

--- src/backend/storage/api/private/router.py
+++ src/backend/storage/api/private/router.py
@@ -4,51 +4,70 @@ from typing import List
 from fastapi import APIRouter, status, UploadFile, File, Form
 
 from src.backend.storage.api.dependencies import minio_client_dependency
-from src.backend.storage.api.private.schemas import PrivateUploadResponse
+from src.backend.storage.api.private.schemas import (
+    UserUploadRequest,
+    UserUploadResponse,
+    UserUploadUpdateRequest,
+    UserUploadUpdateResponse,
+    UserUploadDeleteRequest,
+)
 import src.backend.storage.api.private.service as service
 
 __all__ = [
     "router",
 ]
 
-router = APIRouter(tags=["private"])
+router = APIRouter(tags=["User"])
 
 
 @router.post(
-    "/upload",
+    "/files",
     status_code=status.HTTP_202_ACCEPTED,
-    response_model=PrivateUploadResponse,
+    response_model=UserUploadResponse,
 )
-def upload(
+def private_upload(
     minio_client: minio_client_dependency,
+    request: UserUploadRequest = Form(...),
     files: List[UploadFile] = File(...),
-    user_id: UUID = Form(),
-    chat_id: UUID = Form(),
 ):
     return service.s3_batch_upload(
         minio_client=minio_client,
+        user_id=request.user_id,
+        chat_id=request.chat_id,
         files=files,
-        user_id=user_id,
-        chat_id=chat_id,
     )
 
 
 @router.put(
-    "/upload",
+    "/files",
     status_code=status.HTTP_202_ACCEPTED,
-    response_model=PrivateUploadResponse,
+    response_model=UserUploadUpdateResponse,
 )
-def upload(
+def private_update(
     minio_client: minio_client_dependency,
+    request: UserUploadUpdateRequest = Form(...),
     files: List[UploadFile] = File(...),
-    file_ids: List[UUID] = Form(),
-    user_id: UUID = Form(),
-    chat_id: UUID = Form(),
 ):
     return service.s3_batch_upload(
         minio_client=minio_client,
-        user_id=user_id,
-        chat_id=chat_id,
+        user_id=request.user_id,
+        chat_id=request.chat_id,
         files=files,
-        file_ids=file_ids,
+        file_ids=request.file_ids,
+    )
+
+
+@router.delete(
+    "/files",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+def private_delete(
+    minio_client: minio_client_dependency,
+    request: UserUploadDeleteRequest,
+):
+    return service.s3_batch_delete(
+        minio_client=minio_client,
+        user_id=request.user_id,
+        chat_id=request.chat_id,
+        file_ids=request.file_ids,
     )
--- src/backend/storage/api/private/schemas.py
+++ src/backend/storage/api/private/schemas.py
@@ -1,16 +1,184 @@
-from typing import List
+from enum import Enum
+from typing import List, Optional, Any
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, model_validator
+from pydantic_core import from_json
 
-
-__all__ = [
-    "PrivateUploadResponse",
+_all_ = [
+    "UserUploadRequest",
+    "UserUploadResponse",
+    "UserUploadUpdateRequest",
+    "UserUploadUpdateResponse",
+    "StreamUploadRequest",
+    "StreamUploadResponse",
+    "ObjectOperation",
 ]
 
 
-class PrivateUploadResponse(BaseModel):
-    user_id: UUID
-    chat_id: UUID
-    task_ids: List[UUID]
-    file_ids: List[UUID]
+class ObjectOperation(str, Enum):
+    CREATE = "create"
+    MODIFY = "modify"
+    DELETE = "delete"
+
+
+# -------------------------------- User Uploads ----------------------------------------
+class UserUploadRequest(BaseModel):
+    user_id: UUID = Field(
+        title="User ID",
+        description="""
+            A unique identifier of the user.
+            Will be used to form the bucket name""",
+    )
+    chat_id: UUID = Field(
+        title="Chat Session ID",
+        description="""
+        A unique identifier for the chat ID.
+        Will be used to form the bucket name
+        """,
+    )
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_to_json(cls, data: Any) -> Any:
+        if isinstance(data, str):
+            return from_json(data)
+        return data
+
+
+class UserUploadResponse(BaseModel):
+    file_ids: List[UUID] = Field(
+        title="Documents IDs",
+        description="""
+        A list of documents stashed at the S3 bucket.
+        """,
+    )
+
+    chat_id: UUID = Field(
+        title="Chat Session ID",
+        description="A unique identifier for the chat session.",
+    )
+
+    user_id: UUID = Field(
+        title="User ID",
+        description="A unique identifier of the user.",
+    )
+
+    task_ids: List[UUID] = Field(
+        title="Task IDs",
+        description="A list of unique identifiers for each upload task.",
+    )
+
+
+class UserUploadUpdateRequest(BaseModel):
+    # List of document ID (UUID) that to be ingested and upserted to the vectorstore
+    file_ids: List[UUID] = Field(
+        title="Document IDs",
+        description="List of UUID identifiers of updated documents",
+    )
+
+    # Chat ID - UUID format
+    chat_id: UUID = Field(
+        title="Chat Session ID",
+        description="""
+        A unique identifier for the chat ID.
+        Will be used to form the bucket name
+        """,
+    )
+
+    # a UUID for user_id
+    user_id: UUID = Field(
+        title="User ID",
+        description="""
+            A unique identifier of the user.
+            Will be used to form the bucket name""",
+    )
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_to_json(cls, data: Any) -> Any:
+        if isinstance(data, str):
+            return from_json(data)
+        return data
+
+
+class UserUploadUpdateResponse(BaseModel):
+    # List of document ID (UUID) that to be ingested and upserted to the vectorstore
+    file_ids: List[UUID] = Field(
+        title="Document IDs",
+        description="List of successful UUID identifiers of upserted documents",
+    )
+
+    # Chat ID - UUID format
+    chat_id: UUID = Field(
+        title="Chat ID",
+        description="UUID identifier of the chat to which the documents belong to",
+    )
+
+    user_id: UUID = Field(
+        title="User ID",
+        description="UUID identifier for the user",
+    )
+
+    task_ids: List[UUID] = Field(
+        title="Task IDs",
+        description="A list of unique identifiers for each upload task.",
+    )
+
+
+class UserUploadDeleteRequest(BaseModel):
+    file_ids: List[UUID] = Field(
+        title="Document IDs",
+        description="""List of UUID identifiers of deleted documents.
+        A comma separated list is expected.""",
+        default_factory=list,
+    )
+
+    # a UUID for user_id
+    user_id: UUID = Field(title="User ID", description="UUID identifier for the user")
+
+    # Chat ID - UUID format
+    # if not supplied, will be created on the fly
+    # and thus create a new chat (not recommended)
+    chat_id: UUID = Field(
+        title="Chat ID",
+        description="UUID identifier for the chat",
+    )
+
+
+# -------------------------------- User Uploads ----------------------------------------
+
+
+# ------------------------------ Stream Uploads ----------------------------------------
+# For request coming from Kafka
+# Usually captured by a CDC process monitoring the system
+class StreamUploadRequest(BaseModel):
+    path: str = Field(
+        title="File Path",
+        description="Path to a file stored in the filesystem",
+    )
+    namespace: str = Field(
+        title="Namespace",
+        description="""The namespace to which the file belongs.
+        Might be a project name, department, etc.""",
+    )
+    reason: str = Field(
+        title="Event Reason",
+        description="The reason for sending the file event - modified/deleted",
+    )
+    content_type: Optional[str] = Field(
+        title="File Content Type",
+        description="Optional Content-Type header",
+        default=None,
+    )
+
+
+class StreamUploadResponse(BaseModel):
+    bucket_name: str = Field(
+        title="Bucket Name",
+        description="The name of the bucket where the file is stashed",
+    )
+    object_name: str = Field(
+        title="Object Name",
+        description="The name of the S3 object to which the file is stashed to",
+    )
